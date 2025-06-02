b63fb6cf5b8446cd4887becc360809a5c711d63d
v4n3k <ivan71012@icloud.com>
2025-06-02 23:40:09 +0300

    style(message): change colors and border radiuses

--- src/entities/message/ui/MessageItem.tsx
+++ src/entities/message/ui/MessageItem.tsx
@@ -2,14 +2,14 @@ import type { Message } from '@entities/model/types';
 
 export const MessageItem = ({ from, content }: Message) => {
 	const fromStyles = {
-		model: 'bg-sky-500 mr-auto',
-		user: 'bg-slate-500 ml-auto',
+		model: 'bg-sky-600 text-slate-900 mr-auto',
+		user: 'bg-slate-500 text-slate-900 ml-auto',
 		error: 'bg-red-500',
 	};
 
 	return (
 		<li
-			className={`flex max-w-3/4 w-fit py-2 px-3 rounded-lg text-lg ${fromStyles[from]}`}
+			className={`flex max-w-3/4 w-fit py-2 px-3 rounded-xl text-lg ${fromStyles[from]}`}
 		>
 			<p>{content}</p>
 		</li>
